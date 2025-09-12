66f03c2ebb50d444700b6a44967928000b7801bc
mano299 <mano299ahmed1@gmail.com>
2025-09-12 10:57:17 +0300

    crearte shaped container

--- lib/features/check_out/presentation/views/thank_you_view.dart
+++ lib/features/check_out/presentation/views/thank_you_view.dart
@@ -6,6 +6,9 @@ class ThankYouView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ThankYouViewBody();
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: ThankYouViewBody(),
+    );
   }
 }
--- lib/features/check_out/presentation/widget/thank_you_view_body.dart
+++ lib/features/check_out/presentation/widget/thank_you_view_body.dart
@@ -5,6 +5,53 @@ class ThankYouViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xffD9D9D9),
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            Positioned(
+              top: -50,
+              right: 0,
+              left: 0,
+              child: CircleAvatar(
+                radius: 50,
+                backgroundColor: Color(0xffD9D9D9),
+                child: CircleAvatar(
+                  radius: 40,
+                  backgroundColor: Color(0xff34A853),
+                  child: Icon(
+                    Icons.check,
+                    size: 64,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              left: -20,
+              bottom: MediaQuery.sizeOf(context).height * .2,
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+              ),
+            ),
+            Positioned(
+              right: -20,
+              bottom: MediaQuery.sizeOf(context).height * .2,
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
