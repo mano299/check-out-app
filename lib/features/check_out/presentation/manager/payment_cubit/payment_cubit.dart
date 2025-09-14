bf32348c81f8cbfbb284cfa2c4d268ec44f6aa0d
mano299 <mano299ahmed1@gmail.com>
2025-09-14 11:37:14 +0300

    create payment cubit

---
+++ lib/features/check_out/presentation/manager/payment_cubit/payment_cubit.dart
@@ -0,0 +1,26 @@
+import 'package:bloc/bloc.dart';
+import 'package:check_out_app/features/check_out/data/repos/checkout_repo.dart';
+import 'package:meta/meta.dart';
+
+import '../../../data/models/payment_intent_input_model.dart';
+
+part 'payment_state.dart';
+
+class PaymentCubit extends Cubit<PaymentState> {
+  PaymentCubit(this.checkoutRepo) : super(PaymentInitial());
+
+  final CheckoutRepo checkoutRepo;
+
+  Future makePayment(
+      {required PaymentIntentInputModel paymentIntentInputModel}) async {
+    emit(PaymentLoading());
+
+    var data = await checkoutRepo.makePayment(
+        paymentIntentInputModel: paymentIntentInputModel);
+
+    data.fold(
+      (l) => emit(PaymentFailure(l.errMessage)),
+      (r) => emit(PaymentSuccess()),
+    );
+  }
+}
