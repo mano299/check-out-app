5ff8d41b3a6967b3f811c88f9bb812fa0b44ab63
mano299 <mano299ahmed1@gmail.com>
2025-09-14 11:57:23 +0300

    add publishable key

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:check_out_app/core/utils/api_keys.dart';
 import 'package:check_out_app/features/check_out/presentation/views/my_cart_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 void main() {
+  Stripe.publishableKey = ApiKeys.publishableKey;
   runApp(const CheckOutApp());
 }
 
