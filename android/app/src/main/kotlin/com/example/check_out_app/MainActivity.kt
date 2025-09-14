1c0747963e5514b86aff24a45d8e8f24a6ccc0ff
mano299 <mano299ahmed1@gmail.com>
2025-09-14 09:15:52 +0300

    add flutter stripe package

--- android/app/src/main/kotlin/com/example/check_out_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/check_out_app/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.check_out_app
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity(){
+    
+}
