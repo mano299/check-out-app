8a87ddc15884e485491a41f79ad872953a7cceac
mano299 <mano299ahmed1@gmail.com>
2025-09-14 10:40:20 +0300

    imolement create payment intent

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,3 +1,19 @@
+import 'package:check_out_app/core/utils/api_keys.dart';
+import 'package:check_out_app/core/utils/api_service.dart';
+import 'package:check_out_app/features/check_out/data/models/payment_intent_input_model.dart';
+import 'package:check_out_app/features/check_out/data/models/payment_intent_model/payment_intent_model.dart';
+
 class StripeService {
-  
+  final ApiService apiService = ApiService();
+  Future<PaymentIntentModel> createPaymentIntent(
+      PaymentIntentInputModel paymentIntentInputModel) async {
+    var response = await apiService.post(
+      url: 'https://api.stripe.com/v1/payment_intents',
+      body: paymentIntentInputModel.toJson(),
+      token: ApiKeys.secretKey,
+    );
+    PaymentIntentModel paymentIntentModel =
+        PaymentIntentModel.fromJson(response.data);
+    return paymentIntentModel;
+  }
 }
