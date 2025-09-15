1add199ebb7966a2c6dbab43a78503f46769b59b
mano299 <mano299ahmed1@gmail.com>
2025-09-15 12:35:30 +0300

    handle stripe error

--- lib/features/check_out/data/repos/checkout_repo_impl.dart
+++ lib/features/check_out/data/repos/checkout_repo_impl.dart
@@ -4,6 +4,7 @@ import 'package:check_out_app/core/utils/stripe_service.dart';
 import 'package:check_out_app/features/check_out/data/models/payment_intent_input_model.dart';
 
 import 'package:dartz/dartz.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 import 'checkout_repo.dart';
 
@@ -16,6 +17,10 @@ class CheckoutRepoImpl implements CheckoutRepo {
       await stripeService.makePayment(
           paymentIntentInputModel: paymentIntentInputModel);
       return right(null);
+    } on StripeException catch (e) {
+      return left(ServiceFailure(
+          errMessage:
+              e.error.message ?? 'Oops there is an error, try again later'));
     } catch (e) {
       return left(ServiceFailure(errMessage: e.toString()));
     }
